332a16b67fe7f6359da32e2a4733a2f865f2a87e
JaewookMun <mun1103.dev@gmail.com>
2025-09-24 20:56:22 +0900

    commit first

---
+++ src/test/java/app/jaewook/stockmanager/StockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jaewook.stockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
