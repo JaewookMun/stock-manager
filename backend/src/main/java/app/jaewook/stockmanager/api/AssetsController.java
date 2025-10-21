849166a01393094c05df6a0110ce2c6b030d79e8
JaewookMun <mun1103.dev@gmail.com>
2025-10-21 20:46:12 +0900

    add: backend api spe

---
+++ backend/src/main/java/app/jaewook/stockmanager/api/AssetsController.java
@@ -0,0 +1,19 @@
+package app.jaewook.stockmanager.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assets")
+public class AssetsController {
+
+    /**
+     * TODO: GET /api/assets/realized-pnl
+     * 실현손익 기록 조회
+     */
+
+    /*
+     * TODO: GET /api/assets/cash-flow
+     * 이체내역 조회 (현금흐름)
+     */
+}
---
+++ backend/src/main/java/app/jaewook/stockmanager/api/StocksController.java
@@ -0,0 +1,14 @@
+package app.jaewook.stockmanager.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StocksController {
+
+    /**
+     * TODO: GET /api/stocks/screen
+     * 상장기업 조건 검색 API
+     */
+}
